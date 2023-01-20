f3cd1043e268b478c9f4f2edb6505c8a310743cb
BrunodevBandeira <brunodev29ab@gmail.com>
2023-01-20 13:53:05 -0300

    Add effect CSS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./components/Main";
 const App = ({init}) => {
 
   const [load, setLoad] = React.useState(false);
+  
     React.useEffect(() => {
       setLoad(true);
     }, []);
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,13 @@ import main from "../mocks/dados";
 
 const Main = ({init}) => {
 
-
   return (
-    // <header className={ init ? `${styles.black}` : "" }>
     <header>
       <section className={`${styles.container}`}>
-        <div className={ init  ?  `${styles.cima}` : `${styles.contentCima}`}></div>
+        <div className={ init ? `${styles.cima}` : `${styles.contentCima}`}></div>
             {
               main.map(({nome}) => (
-                <div className={styles.cell}>
+                <div className={ init ? `${styles.div}` : `${styles.hide}` }>
                     <p className={ init ? `${styles.text}` : `${styles.hide}` }> 
                         {nome}  
                     </p>
