95a4e50c2afe3f0a096b5c11eb9fe67ab5448155
BrunodevBandeira <brunodev29ab@gmail.com>
2023-01-21 01:17:26 -0300

    Bug

--- src/components/Main.js
+++ src/components/Main.js
@@ -31,7 +31,7 @@ const Main = ({init}) => {
               <button id="opa" className={styles.btn}  onClick={() => setIsModalVisible(true)}> 
                       Sobre 
               </button>
-              {true ? (<Modal onClose={() => {setIsModalVisible(false)}}> 
+              {isModalVisible ? (<Modal onClose={() => {setIsModalVisible(false)}}> 
                                 <h2> 
                                 Formado em Engenharia da computação e pelo prestigiado Bootcamp da Generation Brasil como Desenvolvedor FullStack Java , Atuo na area de desenvolvimento a 2 anos, fui monitor de sala, ajudando os alunos a resolver as listas de algoritmo e estrutura de dados em Java e bando de dados MySQL. Em 2020 conclui meu Bootcamp pelo IGTI e pela Digital Inovation One focados no Frontend para finalizar, escolhi a área de TI pois gosto de desafios, tecnologia, empreendedorismo e da liberdade de criação que essa área tem. 
                                 </h2>
