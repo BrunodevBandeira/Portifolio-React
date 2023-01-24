023cb273debc60b3662ec3a7d21ebe005468ae64
BrunodevBandeira <brunodev29ab@gmail.com>
2023-01-24 19:00:42 -0300

    Add js portifolio

--- src/components/Projetos.js
+++ src/components/Projetos.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Modal from "./Modal";
 import styles from "./Projetos.module.scss";
+import { dadosNode } from "../mocks/portifolioDados";
 
 
 const Projetos = () => {
@@ -12,11 +13,50 @@ const Projetos = () => {
 
         <div>
           <button className={styles.btn} onClick={() => setIsModalVisible(true)}> Projetos </button>
-          {isModalVisible ? (<Modal  onClose={() => {setIsModalVisible(false)}}> 
-                                <h2> 
-                              Projetos... 
-                                </h2>
-                            </Modal> ): null }
+          {true ? (<Modal  onClose={() => {setIsModalVisible(false)}}> 
+                      <section className={styles.portfolio} id="projetos">
+                          <div className={styles.stylesMainText}>
+                              <h2><span> Meus </span> Projetos </h2>
+                              <div className="{styles.mainTextMenu}  + js-tabmenu">
+
+                                <p className={styles.reactreact} id="React"> 			
+                                  React 
+                                </p>
+
+                                <p className={styles.Css} id="Css"> 				
+                                  Css 
+                                </p>
+
+                                <p className={styles.Javascript} id="Javascript">  
+                                  Javascript 
+                                </p>
+
+                                <p className={styles.Node} id="Node"> 				
+                                  Node 
+                                </p>
+
+                              </div>
+                          </div>
+
+                          {/* {dadosNode.map(({image, title, text, link}, id) => (
+                            // <div className="{styles.portfolioContent} js-tabcontent">
+                            <div className={styles.portfolioContent} key={id}>
+                              <section >
+                                  <div className={styles.row} id="node">
+                                    <img  className={styles.img} src={image} />
+                                    <div className={styles.layer}>
+                                      <h5> {title} </h5>
+                                      <p> {text} </p>
+                                      <a href={link} target="_blank">
+                                        <i className='bx bx-link-external' ></i>
+                                      </a>
+                                    </div>
+                                  </div>
+                                </section>
+                            </div>
+                          ))} */}
+                      </section>
+                    </Modal> ): null }
         </div>
 
       </section>
---
+++ src/mocks/portifolioDados.js
@@ -0,0 +1,18 @@
+export const dadosNode = [
+    {
+      id: 1,
+      image: "../assets/art.jpg",
+      title: "React Portifólio",
+      text: "Projeto desenvolvido com Node e Typescript...",
+      link: "https://github.com/BrunodevBandeira/nodeJs-lendo-tabela-excel"
+    },
+    {
+        id: 2,
+        image: "../assets/pokemon.jpg",
+        title: "Pokemon Node",
+        text: "Projeto desenvolvido com Node...",
+        link: "https://github.com/BrunodevBandeira/Node-Pokemon"
+      }
+  ];
+
+// import foto from "../../public/assets/art.jpg"
